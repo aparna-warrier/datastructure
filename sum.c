d52a9c185955f293425f8ba79fd8a274c2f0343c
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-11-23 11:02:36 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+clrscr();
+int i,limit,sum=0;
+printf("Enter the limit:"\n);
+scanf("%d",&limit);
+for(i=0;i<n;i++)
+{
+sum=sum+i;
+}
+printf("\n Sum of elements %d",%sum);
+}
